c716a18d84e6ddc68ce127a6956c98092180ed14
Camille Scott <camille.scott.w@gmail.com>
2014-08-05 02:47:20 -0400

    Add UniProt querying

--- dodo.py
+++ dodo.py
@@ -1,36 +1,44 @@
-from peasoup.tasks import BlastTask, BlastFormatTask, CurlTask, GunzipTask
+from peasoup.tasks import BlastTask, BlastFormatTask, CurlTask, GunzipTask, \
+                            UniProtQueryTask
 
 blast_params = '-best_hit_score_edge 0.05 -best_hit_overhang 0.25 -max_target_seqs 1'
 blast_threads = 8
 
-assem_url = ('athyra.ged.msu.edu/~cswelcher/lamp10.fasta.gz', 'lamp10.fasta.gz')
+assem_url = ('http://athyra.ged.msu.edu/~cswelcher/lamprey/lamp10.fasta', 'lamp10.fasta.gz')
 assem = assem_url[1].rstrip('.gz')
 
 db_urls = [
-('http://hgdownload.soe.ucsc.edu/goldenPath/petMar2/bigZips/petMar2.fa.gz ','petMar2.fa.gz'),
-#('http://hgdownload.soe.ucsc.edu/goldenPath/petMar2/bigZips/petMar2.fa.masked.gz', 'petMar2.fa.masked.gz'),
+('ftp://ftp.ensembl.org/pub/release-75/fasta/petromyzon_marinus/dna/Petromyzon_marinus.Pmarinus_7.0.75.dna_sm.toplevel.fa.gz','petMar2.fa.gz'),
 ('http://hgdownload.soe.ucsc.edu/goldenPath/petMar2/bigZips/est.fa.gz', 'petMar2.est.fa.gz'),
-('http://hgdownload.soe.ucsc.edu/goldenPath/petMar2/bigZips/mrna.fa.gz', 'petMar2.mrna.fa.gz'),
-('ftp://ftp.ensembl.org/pub/release-72/fasta/petromyzon_marinus/cdna/Petromyzon_marinus.Pmarinus_7.0.72.cdna.all.fa.gz', 'petMar2.cdna.fa.gz'),
-('ftp://ftp.ensembl.org/pub/release-71/fasta/mus_musculus/pep/Mus_musculus.GRCm38.71.pep.all.fa.gz','musMus.pep.fa.gz'),
-('ftp://ftp.ensembl.org/pub/release-71/fasta/danio_rerio/pep/Danio_rerio.Zv9.71.pep.all.fa.gz','danRer.pep.fa.gz'),
+('ftp://ftp.ensembl.org/pub/release-75/fasta/petromyzon_marinus/pep/Petromyzon_marinus.Pmarinus_7.0.75.pep.all.fa.gz', 'petMar2.pep.fa.gz'),
+('ftp://ftp.ensembl.org/pub/release-75/fasta/petromyzon_marinus/cds/Petromyzon_marinus.Pmarinus_7.0.75.cds.all.fa.gz', 'petMar2.cds.fa.gz'),
+('ftp://ftp.ensembl.org/pub/release-75/fasta/petromyzon_marinus/ncrna/Petromyzon_marinus.Pmarinus_7.0.75.ncrna.fa.gz', 'petMar2.ncrna.fa.gz'),
+('ftp://ftp.ensembl.org/pub/release-75/fasta/petromyzon_marinus/cdna/Petromyzon_marinus.Pmarinus_7.0.75.cdna.all.fa.gz', 'petMar2.cdna.fa.gz'),
+('ftp://ftp.ensembl.org/pub/release-75/fasta/mus_musculus/pep/Mus_musculus.GRCm38.75.pep.all.fa.gz','musMus.pep.fa.gz'),
+('ftp://ftp.ensembl.org/pub/release-75/fasta/danio_rerio/pep/Danio_rerio.Zv9.75.pep.all.fa.gz' ,'danRer.pep.fa.gz'),
 ('ftp://ftp.ncbi.nih.gov/pub/UniVec/UniVec_Core','uniVec.fa'),
-('f#tp://ftp.ensembl.org/pub/release-74/fasta/homo_sapiens/pep/Homo_sapiens.GRCh37.74.pep.all.fa.gz','homSap.pep.fa.gz'),
-('ftp://ftp.ensembl.org/pub/release-74/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh37.74.cdna.all.fa.gz','homSap.cdna.fa.gz'),
+#('ftp://ftp.ensembl.org/pub/release-74/fasta/homo_sapiens/pep/Homo_sapiens.GRCh37.74.pep.all.fa.gz','homSap.pep.fa.gz'),
+#('ftp://ftp.ensembl.org/pub/release-74/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh37.74.cdna.all.fa.gz','homSap.cdna.fa.gz'),
 ]
+
+uniprot_terms = [('organism:7739+AND+keyword:1185', 'braFlo.pep.all.fa.gz'),
+                 ('taxonomy:7762', 'Myx.pep.all.fa.gz')]
 
 #
 # Get the reference databases
 #
 
-get_dbs = CurlTask(db_urls.append(assem_url))
+get_dbs = CurlTask(db_urls + [assem_url])
+uniprot_tasks = [UniProtQueryTask(org_id, fn) for org_id, fn in uniprot_terms]
 
 #
 # Gunzip the downloaded databases
 #
-
+uniprot_dbs = [gz.outputs().next() for gz in uniprot_tasks]
+print uniprot_dbs
 gunzip_dbs = GunzipTask([(src,src.rstrip('.gz')) for src in get_dbs.outputs() \
-                    if src.endswith('.gz')])
+                    if src.endswith('.gz')] + \
+                [(src,src.rstrip('.gz')) for src in uniprot_dbs])
 
 #
 # run makeblastdb on downloaded databases
@@ -40,13 +48,16 @@ mkdb_tasks = []
 for src in gunzip_dbs.outputs():
     db_type='prot' if 'pep' in src else 'nucl'
     mkdb_tasks.append(BlastFormatTask(src, '{}.db'.format(src), db_type))
-mkdb_tasks.append(BlastFormatTask(assem, '{}.db'.format(assem), 'nucl'))
+#mkdb_tasks.append(BlastFormatTask(assem, '{}.db'.format(assem), 'nucl'))
 
 def task_prep_databases():
     global databases
 
 
     yield get_dbs.tasks()
+    for task in uniprot_tasks:
+        yield task.tasks()
+
     yield gunzip_dbs.tasks()
 
     for task in mkdb_tasks:
